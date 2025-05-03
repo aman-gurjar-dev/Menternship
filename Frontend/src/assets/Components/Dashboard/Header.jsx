4b29a79063dccd03c00c0c226ef19630731d83a2
aman-gurjar-dev <amangurjar160@gmail.com>
2025-05-03 15:56:57 +0530

    Merge branch

--- Frontend/src/assets/Components/Dashboard/Header.jsx
+++ Frontend/src/assets/Components/Dashboard/Header.jsx
@@ -8,15 +8,15 @@ const Header = ({ userData, handleLogout }) => {
       initial={{ y: -50, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ duration: 0.5 }}
-      className="text-white p-6 flex justify-between items-center shadow-md"
+      className="text-white p-6 flex justify-end items-center shadow-md"
     >
-      <h2 className="text-2xl font-bold">
+      <h2 className="text-2xl font-bold hidden md:block absolute left-6">
         Welcome,<br />
-        <span className="font-extrabold text-black">{userData?.name || "User"}</span>
+        <span className="text-white">{userData?.name || "User"}</span>
       </h2>
       <button
         onClick={handleLogout}
-        className="bg-red-500 px-4 py-2 rounded-lg hover:bg-red-800 cursor-pointer"
+        className="bg-red-500 px-4 py-2 rounded-lg hover:bg-red-800 cursor-pointer text-white"
       >
         Logout
       </button>
